5b7101cf197c5392acb7807f1c22b98321be8605
Tommaso <tommaso@MacBook-Pro-di-Tommaso.local>
2017-11-07 10:28:42 +0100

    asks for directory and creates it

--- utildown.py
+++ utildown.py
@@ -13,7 +13,10 @@ for link in soup.find_all('a'):
     urls.append("http://www.dmmm.uniroma1.it/~romano.scozzafava/testiesame/" + link.get('href'))
 urls = urls[5:urls.__len__()]   # cleaning urls
 print(urls)
-directory= input('Write the path\n')
+directory = input('write the directory where I can download compitis\n')
+directory_path= os.path.join(os.getcwd(),directory)
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
 pattern = '(?<=testiesame/).+pdf'   # using regex makes me feel so smart
 for url in urls:    # finding compiti's name
     names.append(re.findall(pattern, url))
